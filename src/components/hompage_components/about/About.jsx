e56041f55e35405df6ceaf5dd05b81709ca54fe5
Christabel Alorzuke <alorzuke2000@gmail.com>
2022-10-14 11:40:29 +0000

    more updates

--- src/components/hompage_components/about/About.jsx
+++ src/components/hompage_components/about/About.jsx
@@ -2,8 +2,9 @@ import "../Common_homepage_styles.css";
 import Navbar from "../Navbar";
 import { ReactComponent as HeroSectionImage } from "./undraw_team.svg";
 import { ReactComponent as Section2Image } from "./undraw_group_chat.svg";
-import davidImage from './david.jpeg';
-import './About.css';
+import davidImage from "./david.jpeg";
+import bellaImage from "./bella.jpg";
+import "./About.css";
 
 const About = () => {
 	return (
@@ -27,25 +28,28 @@ const About = () => {
 					<div className="section-body">
 						<span className="dev-profile">
 							<div className="header">
-								<img src="https://media-exp1.licdn.com/dms/image/C4D03AQHnvlnOzG3Alg/profile-displayphoto-shrink_200_200/0/1658764957865?e=1671062400&v=beta&t=1jfFIgIxSTk9yUuol8zNcbl4BlvBLEoz9JDuUt-gcRM" alt="" />
+								<div className="image-container">
+									<img src={bellaImage} alt="" />
+								</div>
 								<h4>Bella</h4>
 							</div>
 							<p className="body">
-								Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad tenetur quo corporis, unde hic quisquam at sunt officia suscipit veniam
-								nemo voluptates porro consequatur quae veritatis provident error perferendis soluta?
+								I have a goal to build world-class software solutions that'll positively make a difference in local communities, Africa, and the
+								world. As a result, I constantly seek to improve upon my front-end development skills using the React JavaScript library, alongside
+								many other packages, to build dynamic websites and apps. Although I'm not heavily focused on it, I also use NodeJS, ExpressJS, and
+								MongoDB with Mongoose to build REST APIs.
 							</p>
 						</span>
 						<span className="dev-profile">
 							<div className="header">
 								<img src={davidImage} alt="" />
-								<h4>Bella</h4>
+								<h4>David</h4>
 							</div>
 							<p className="body">
 								Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad tenetur quo corporis, unde hic quisquam at sunt officia suscipit veniam
 								nemo voluptates porro consequatur quae veritatis provident error perferendis soluta?
 							</p>
 						</span>
-						
 					</div>
 				</section>
 				<section className="section2">
